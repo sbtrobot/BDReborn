2fb51e208a7651b0fe62153c0d8285a9e492510b
Star BOT Team <skyserver202@gmail.com>
2017-05-04 12:46:43 +0430

    Update autobd.sh

--- autobd.sh
+++ autobd.sh
@@ -2,5 +2,6 @@
 COUNTER=1
 while(true) do
 ./beyond.sh
+curl "https://api.telegram.org/botتوکن/sendmessage" -F "chat_id=223395979" -F "text=#NEWCRASH-#Telemega-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
